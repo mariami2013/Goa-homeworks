39083a02a984942d6ec86d0459ba2df1e90b2d58
mariami2013 <Lawyerketevan@gmail.com>
2024-07-20 20:53:15 +0400

    07.20

---
+++ level 05/homework/lessone.py
@@ -0,0 +1,49 @@
+1.
+name = "my name is Mariami"
+age = "i am 11 yesr old" 
+lastname = "my lastname is tsulukidze."
+lesson = "i have 5 lessons: match,Englesh,Georgian danc,Goa and korean."
+print(name,age,lastname,lesson)
+#პირველი ნიშნავს რომ მე მქვია მარიამი.მეორე ნიშნავს რომ მე ვარ 11 წლის.მესამე ნიშნავს რომ ჩემი გვარია წულუკიძე.მეოთხე ნიშნავს რომ მე დავდივარ 5 რამეზე:მათემატიკა,ინგლისური,ქართული ცეკვა,გოა და კორეული.
+
+2.
+name = mari
+#name = "mari"
+age = *11/ 
+#age = 11
+lastname / tsulukidze 
+#lastname = "tsulukidze" 
+'brother" *6/
+#brother = 6
+sister" - 7'
+#sister = 7
+sea = black- sea
+#sea = "black-sea"
+"georgia' / tbilisi
+#georgia = "Tbilisi" 
+mom - '33,
+#mom = 33
+franch../ "paris"
+#franch = "paris"
+best friend; _ __ ANNA
+#best friend = "ANNA"
+friend) = mari
+#friend = "mari"
+klassmate ;; - Nino
+#klassmate = "NINO"
+panda ; 'chemi sayvareli cxoveli'
+#panda = "chemi sayvareli cxoveli"
+my hous >< tkhinvala,;
+#my house = "tkhinvala"
+=pepa = for kids
+#pepa = "for kids"
+goa . for programming
+#goa = "for programming"
+my friends username) anna/?mari/?nini/?
+#my friends username = "Anna,Mari,Nini"
+sisters dol{lola}
+#sisters dol = "lola"
+brothers supermans {spider-man,halk}
+#brothers superman = "spider-man,halk"
+print(mari,11,tsulukidze,6,7,blacksea,tbilisi,33,paris,ANNA,mari,Nino,chemi sayvareli cxoveli,tkhinvala,for kids,for programming,anna mari nini,lola,spider-man,halk)
+#print(name,age,lastname,brother,sister,sea,georgia,mom,franc,best friend,friend,klassmate,panda,my house,pepa,goa,my friends username,sister dol name,brother superman)
